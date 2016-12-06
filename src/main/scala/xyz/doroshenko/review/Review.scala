8b98b7dad81cf233a4622c2368a4a5bb0b4bff0b
Maksym Doroshenko <mdoroshenko@intropro.com>
2016-12-06 17:24:21 +0200

    Refactor

--- src/main/scala/xyz/doroshenko/review/Review.scala
+++ src/main/scala/xyz/doroshenko/review/Review.scala
@@ -25,18 +25,35 @@ object Review {
       val splitted = line.split(",")
       Review(splitted(1), splitted(3), splitted(9))
     }
-    val mostActiveUsers = reviews.map(review => (review.ProfileName, 1)).reduceByKey(_ + _).takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
-    val mostCommentedItems = reviews.map(review => (review.ProductId, 1)).reduceByKey(_ + _).takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
-    val mostUsedWords = reviews.flatMap(review => review.Text.split("\\W+")).map(word => (word.toLowerCase, 1)).reduceByKey(_ + _).takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
+
+    val mostActiveUsers = reviews
+      .map(review => (review.ProfileName, 1))
+      .reduceByKey(_ + _)
+      .takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
+
+    val mostCommentedItems = reviews
+      .map(review => (review.ProductId, 1))
+      .reduceByKey(_ + _)
+      .takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
+
+    val mostUsedWords = reviews
+      .flatMap(review => review.Text.split("\\W+"))
+      .map(word => (word.toLowerCase, 1))
+      .reduceByKey(_ + _)
+      .takeOrdered(itemsNumber)(Ordering[Int].reverse.on(x => x._2))
 
     mostActiveUsers.foreach(x => println(s"User - ${x._1} : ${x._2} comments"))
     mostCommentedItems.foreach(x => println(s"Product id - ${x._1} : ${x._2} comments"))
-    mostUsedWords.foreach(x=> println(s"Word - ${x._1} used ${x._2} times") )
+    mostUsedWords.foreach(x => println(s"Word - ${x._1} used ${x._2} times"))
   }
 
   private def initSparkContext(master: String, appName: String): SparkContext = {
     val conf = new SparkConf().setMaster(master).setAppName(appName)
     new SparkContext(conf)
+  }
+
+  def main(args: Array[String]): Unit = {
+    getStats
   }
 
 }
--- src/main/scala/xyz/doroshenko/review/ReviewDF.scala
+++ src/main/scala/xyz/doroshenko/review/ReviewDF.scala
@@ -11,9 +11,24 @@ object ReviewDF {
   def getStats() = {
     val spark = getSparkSession()
     val inputDf = spark.read.option("header", "true").csv(getClass.getResource("/Reviews.csv").getPath).cache()
-    val mostActiveUsers = inputDf.groupBy("ProfileName").count().orderBy(desc("count")).show(100)
-    val mostCommentedItems = inputDf.groupBy("ProductId").count().orderBy(desc("count")).show(100)
-    val mostUsedWords = inputDf.select("Text").rdd.map(r => r.getAs[String](0)).flatMap(line => line.split("\\W+")).map(word => (word.toLowerCase, 1)).reduceByKey(_ + _).takeOrdered(100)(Ordering[Int].reverse.on(x => x._2))
+
+    val mostActiveUsers = inputDf
+      .groupBy("ProfileName")
+      .count()
+      .orderBy(desc("count")).show(100)
+
+    val mostCommentedItems = inputDf
+      .groupBy("ProductId")
+      .count()
+      .orderBy(desc("count")).show(100)
+
+    val mostUsedWords = inputDf
+      .select("Text").rdd.map(r => r.getAs[String](0))
+      .flatMap(line => line.split("\\W+"))
+      .map(word => (word.toLowerCase, 1))
+      .reduceByKey(_ + _)
+      .takeOrdered(100)(Ordering[Int].reverse.on(x => x._2))
+
     mostUsedWords.foreach(x => println(s"""Word "${x._1}" used ${x._2}"""))
   }
 
